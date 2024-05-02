2c957fbf9757f98fb16700987748289991dcb75c
ShivaSingh17121997 <shivasingh001997@gmail.com>
2024-05-02 14:33:21 +0530

    added

---
+++ zimeappassign/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "left", backgroundColor: "	#02191d", color: "white", padding: "10px", margin: " 0 0 5px " }} >Zime Ai</h1>
+
+      <Home />
+
+    </div>
+  );
+}
+
+export default App;
